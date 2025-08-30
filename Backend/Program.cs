356cb25bb9153a17c20fe3afd0463c377c530a8f
luisinaBender <luisinabender@gmail.com>
2025-08-30 02:19:52 -0300

    Mejorar configuración del DbContext y Swagger
    Se han realizado cambios en la inyección de dependencias del contexto de la base de datos, utilizando `InmobiliariaContext` de `Backend.DataContext`. Se ha optimizado la lectura del archivo `appsettings.json` y se han añadido comentarios para clarificar la configuración de Swagger/OpenAPI en el pipeline de la aplicación.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,24 +1,37 @@
+using Backend.DataContext;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
+        .AddJsonFile("appsettings.json")
+        .Build();
 var cadenaConexion = configuration.GetConnectionString("mysqlRemoto");
 
-builder.Services.AddDbContext<Backend.DataContext.InmobiliariaContext>(options =>
-    options.UseMySql(cadenaConexion,
-    ServerVersion.AutoDetect(cadenaConexion)));
+// configuración de inyección de dependencias del DBContext
+builder.Services.AddDbContext<InmobiliariaContext>(
+    options => options.UseMySql(cadenaConexion,
+                                ServerVersion.AutoDetect(cadenaConexion)));
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
